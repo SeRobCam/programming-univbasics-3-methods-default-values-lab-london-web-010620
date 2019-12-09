12c7c9b66e64baaa72a5def8c76a5a46e3adc43e
Sergio Robles <sroblesc@gmail.com>
2019-12-09 22:06:44 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,10 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
+var protein = "meat"
+
 def meal_choice(veg1, veg2)
   puts "What a nutritious meal!"
-  var protein = "meat"
   if (protein = true)
     puts "A plate of meat and #{veg1} and #{veg2}."
   end
