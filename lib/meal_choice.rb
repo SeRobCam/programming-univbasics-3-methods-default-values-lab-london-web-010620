6bf55bb984677abdca1867d0d130acaeade455e1
Sergio Robles <sroblesc@gmail.com>
2019-12-09 22:01:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,6 +2,8 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1, veg2)
-  
+  var protein = "meat"
+  if (protein = true)
+  puts "What a nutritious meal!"
   
 end
