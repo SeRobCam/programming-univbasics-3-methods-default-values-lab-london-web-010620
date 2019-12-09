1c197d1d689d3548e8bc39a1da1a096d65aa559d
Sergio Robles <sroblesc@gmail.com>
2019-12-09 22:03:34 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,8 +2,11 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1, veg2)
+  puts "What a nutritious meal!"
   var protein = "meat"
   if (protein = true)
-  puts "What a nutritious meal!"
-  
+    puts "A plate of meat and #{veg1} and #{veg2}."
+  else
+    puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  end
 end
