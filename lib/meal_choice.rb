e5359eb4ede545cdf285b48f594c4232b71e2edd
Sergio Robles <sroblesc@gmail.com>
2019-12-09 21:37:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,6 @@
 def meal_choice(veg1, veg2, protein = "meat")
   if (protein == true)
     puts "What a nutritious meal!"
-  if (protein == false)
+  else
     puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
